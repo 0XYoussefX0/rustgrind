5cf973109234d3d8685646bc549c4efe5748db20
Youssef Abelouas <youssef.abelouass@gmail.com>
2024-12-13 00:48:54 +0100

    🎉 Initial commit

---
+++ target/debug/deps/rustgrind-1bd5b6109e3e8656.d
@@ -0,0 +1,5 @@
+/home/youssef/Projects/rustgrind/target/debug/deps/librustgrind-1bd5b6109e3e8656.rmeta: src/main.rs
+
+/home/youssef/Projects/rustgrind/target/debug/deps/rustgrind-1bd5b6109e3e8656.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rustgrind-2a4193d80c56a5f2.d
@@ -0,0 +1,5 @@
+/home/youssef/Projects/rustgrind/target/debug/deps/librustgrind-2a4193d80c56a5f2.rmeta: src/main.rs
+
+/home/youssef/Projects/rustgrind/target/debug/deps/rustgrind-2a4193d80c56a5f2.d: src/main.rs
+
+src/main.rs:
